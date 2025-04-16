6b1a598dd9f0250c72b47298ebec6de228a66bd3
anupmanekar <anup.manekar@nitorinfotech.com>
2025-04-15 22:10:35 -0400

    Module 0 and Module 1

---
+++ module-1/studio/router.py
@@ -0,0 +1,39 @@
+from langchain_fireworks import ChatFireworks
+from langgraph.graph import MessagesState
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
+
+# Tool
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+# LLM with bound tool
+# Tried with llama-v3-8b-instruct and calls were not routed through tools but when used mixtral-8x22b-instruct, calls were successfully routed to tools
+llm = ChatFireworks(model="accounts/fireworks/models/mixtral-8x22b-instruct")
+llm_with_tools = llm.bind_tools([multiply])
+
+# Node
+def tool_calling_llm(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_node("tools", ToolNode([multiply]))
+builder.add_edge(START, "tool_calling_llm")
+builder.add_conditional_edges(
+    "tool_calling_llm",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", END)
+
+# Compile graph
+graph = builder.compile()
